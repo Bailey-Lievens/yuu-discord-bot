d9edbbfc89d983de970df1e44fe56c7e9f9c734f
Bailey Lievens <baileylievens@hotmail.be>
2022-02-20 20:31:38 +0100

    Feat: added bot saying hey there

--- discordbot.js
+++ discordbot.js
@@ -1,10 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS"] })
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
+
+const prefix = "Yuu";
+
+client.on("ready", () => {
+    console.log("Logged in as " + client.user.tag + "!");
+});
+
+client.on("messageCreate", (msg) => {
+
+    //If message doesn't start with prefix return
+    if (!msg.content.startsWith(prefix)) return;
+
+    //Remove prefix from message + 1 for the space
+    let message = msg.content.slice(prefix.length + 1);
+
+    if(message == "hi") {
+        msg.channel.send("Hey there");
+    }
+
+  });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
