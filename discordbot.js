d4c2dea9994e4c970ff08271990c44f455d4df0f
Bailey Lievens <baileylievens@hotmail.be>
2022-02-20 19:46:57 +0100

    Feat: added working bot

--- discordbot.js
+++ discordbot.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS",
-                                            "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING",
-                                            "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING", "GUILD_SCHEDULED_EVENTS"] })
+const client = new Discord.Client({ intents: ["GUILDS"] })
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
